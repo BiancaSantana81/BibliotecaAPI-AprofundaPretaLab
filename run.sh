12431ba20d5b04a44f1f5b05e3040cafcf0fda38
BiancaSantana81 <biancasantana81@hotmail.com>
2025-07-04 18:13:38 -0300

    WIP #projeto03-aprofunda: adicionando um script para facilitar a execução do projeto

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function clean() {
+  echo "Limpando executáveis e node_modules..."
+  find . -type f -executable -exec rm -v {} \;
+  find . -type d -name "node_modules" -exec rm -rfv {} \;
+  echo "Limpeza concluída!"
+}
+
+function setup() {
+  echo "Inicializando o projeto..."
+  npm install
+}
+
+function test() {
+  echo "Rodando testes..."
+  npm test
+}
+
+function start_server() {
+  echo "Iniciando servidor..."
+  npm start
+}
+
+if [ "$1" == "clean" ]; then
+  clean
+else
+  echo "Iniciando projeto..."
+  setup
+  test
+  start_server
+fi
